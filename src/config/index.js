784622ce6e29297f770bac39a04c7a70b3e55296
buchalla <vs_buchalla@hotmail.com>
2023-10-26 18:52:25 -0300

    feat: using envs in config

--- src/config/index.js
+++ src/config/index.js
@@ -9,16 +9,15 @@ const config = {
   // IS_LOCAL: false,
   IS_LOCAL: process.env.NODE_ENV === 'local',
   AUTH: {
-    AUDIENCE: 'https://api.voidr.co/',
-    ISSUER_BASE_URL: 'https://voidr-staging.us.auth0.com/',
-    APP_CLIENT_ID: 'AC7hfmPWWOVcCQnOLvSkuAcJYNwHLAl7',
+    AUDIENCE: process.env.AUTH_AUDIENCE, //'https://api.voidr.co/',
+    ISSUER_BASE_URL: process.env.AUTH_ISSUER_BASE_URL, //'https://voidr-staging.us.auth0.com/',
+    APP_CLIENT_ID: process.env.AUTH_APP_CLIENT_ID, //'AC7hfmPWWOVcCQnOLvSkuAcJYNwHLAl7',
   },
   AUTH_MANAGEMENT: {
-    DOMAIN_URL: 'https://voidr-staging.us.auth0.com/',
-    CLIENT_ID: 'YN5FBum4o9aoCIAuNgZRlsdxcNdcRGhM',
-    CLIENT_SECRET:
-      'dl-l3N8NMObTYymssM204m2Jspt__PNUfdTItmTcKiJlj7A_wtq_QseoUiT52uBS',
-    AUDIENCE: 'https://voidr-staging.us.auth0.com/api/v2/',
+    DOMAIN_URL: process.env.AUTH_MANAGEMENT_DOMAIN_URL, //'https://voidr-staging.us.auth0.com/',
+    CLIENT_ID: process.env.AUTH_MANAGEMENT_CLIENT_ID, //'YN5FBum4o9aoCIAuNgZRlsdxcNdcRGhM',
+    CLIENT_SECRET: process.env.AUTH_MANAGEMENT_CLIENT_SECRET, //'dl-l3N8NMObTYymssM204m2Jspt__PNUfdTItmTcKiJlj7A_wtq_QseoUiT52uBS',
+    AUDIENCE: process.env.AUTH_MANAGEMENT_AUDIENCE, //'https://voidr-staging.us.auth0.com/api/v2/',
   },
   LOGTAIL: {
     TOKEN: 'wLrwxhNntYoAkUU34ejR7owi',
