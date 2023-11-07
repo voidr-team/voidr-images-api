7669567db7402040c3bf5a23aaf9519a83a95bc3
buchalla <vs_buchalla@hotmail.com>
2023-11-07 19:29:09 -0300

    fix: filename

--- src/routes/images/schema.js
+++ src/routes/images/schema.js
@@ -4,7 +4,6 @@ export const uploadSchema = yup.object().shape({
   file: yup
     .string()
     .required('file is required')
-    .matches(/^[a-zA-Z0-9-_]+(\.[a-zA-Z0-9]+)?$/, 'Invalid file name')
     .max(255, 'File name must be at most 255 characters long'),
 
   contentType: yup
